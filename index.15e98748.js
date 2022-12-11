f4cbbba601fb2c4ccf4903a9c4db339bb9adafbf
MarynaSkoryk <108087570+MarynaSkoryk@users.noreply.github.com>
2022-12-11 17:42:00 +0000

    Deploying to gh-pages from @ MarynaSkoryk/widget-weather@41727bd829d09c6800dfe4dc5143debe93dc954a 🚀

---
+++ index.15e98748.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#weather");e&&async function(i){e.innerHTML='<div class="weather__loading"><img src="./images/loading.gif" alt="Loading..."></div>'}();
+//# sourceMappingURL=index.15e98748.js.map
